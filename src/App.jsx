7d13c8eb527bc6a408e6e71229038f004a723909
JensonAquino <jensonaquino2622@gmail.com>
2024-12-23 23:26:51 +0100

    feat: description section

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,18 @@ function App() {
       </header>
       <main className="container my-5">
         {/* btn section */}
-      <div>
-        {languages.map((curLng) => (
-          <button key={curLng.id} className="btn btn-primary me-2">{curLng.title}</button>
-        ))}
-      </div>  
+        <div>
+          {languages.map((curLng) => (
+            <button key={curLng.id} className="btn btn-primary me-2">{curLng.title}</button>
+          ))}
+        </div>
+        {/* description section */}
+        <div className="card mt-5">
+          <div className="card-body">
+          <h3>{languages[0].title}</h3>
+          <p>{languages[0].description}</p>
+          </div>
+        </div>
       </main>
     </>
   )
